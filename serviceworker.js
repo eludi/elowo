b04c2ef955313ce1ca57d300a2317dba0220eacd
Gerald Franz <gf@eludi.net>
2019-02-28 19:08:26 +0100

    editor color contrast increased

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-const version = '043';
+const version = '044';
 const cacheWhitelist = ['static_'+version, 'app'];
 
 this.addEventListener('install', (event)=>{
@@ -21,7 +21,7 @@ this.addEventListener('install', (event)=>{
 			'./applets/pocket%20piano.js',
 			'./applets/zoom_test.js',
 			'./applets/env_test.js',
-			'./doc.html',
+			//'./doc.html',
 			'./doc/api.md',
 			'./doc/create.svg',
 			'./doc/edit.svg',
@@ -43,7 +43,7 @@ this.addEventListener('install', (event)=>{
 			'./icon180.png',
 			'./icon192.png',
 			'./icon256.png',
-			'./icon512.png',
+			'./icon512.png'/*,
 			'./',
 			'./app.js',
 			'./dt.js',
@@ -54,7 +54,7 @@ this.addEventListener('install', (event)=>{
 			'./manifest.json',
 			'./packager.js',
 			'./run.html',
-			'./runtime.js'
+			'./runtime.js'*/
 		]);
 	}));
 });
